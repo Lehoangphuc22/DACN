61964935e370ad0bcca187001061aaaaca779f45
Lehoangphuc22 <lephuc658@gmail.com>
2024-10-08 13:19:33 +0700

    add homepage

---
+++ TES_MEDICAL/TES_MEDICAL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TES_MEDICAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
