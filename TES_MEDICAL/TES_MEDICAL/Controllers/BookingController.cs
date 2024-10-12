5f9e58df0471001896eb81f762e5fdf906003374
Lehoangphuc22 <lephuc658@gmail.com>
2024-10-12 17:24:31 +0700

    12/10

---
+++ TES_MEDICAL/TES_MEDICAL/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TES_MEDICAL.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult BookingBase()
+        {
+            return View();
+        }
+    }
+}
