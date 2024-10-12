c78677833b1a14140d9486ce0e714ea55d7d3574
PhanThanhTu <bin110903@gmail.com>
2024-10-12 20:34:17 +0700

    add doctor

---
+++ TES_MEDICAL/TES_MEDICAL/Controllers/BacsiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TES_MEDICAL.Controllers
+{
+    public class BacsiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Lichsukham()
+        {
+            return View();
+        }
+    }
+}
---
+++ TES_MEDICAL/TES_MEDICAL/Controllers/DuocsiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TES_MEDICAL.Controllers
+{
+    public class DuocsiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dangphat()
+        {
+            return View();
+        }
+        public IActionResult Lichsu()
+        {
+            return View();
+        }
+        public IActionResult Phatthuoc()
+        {
+            return View();
+        }
+    }
+}
